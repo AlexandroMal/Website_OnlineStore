e52ed5cee9311c9e0d033a065bbd9e03675db18d
Alexandr Malina <Sashamalina8695@gmail.com>
2023-02-09 23:16:38 +0200

    created database models for products

--- webstore/models.py
+++ webstore/models.py
@@ -1,8 +1,33 @@
 from django.db import models
 
-class telephonesBase(models.Model):
+class telephones_Base(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
     price = models.CharField(max_length=15)
-    img_full = models.ImageField(upload_to="telephones/full/%Y/%m/%d/")
-    img_contraction = models.ImageField(upload_to="telephones/contraction/%Y/%m/%d/")
+    quantity = models.PositiveIntegerField(default=0)
+    img_full = models.ImageField(upload_to="telephones/")
+   
+    
+class notebook_Base(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    price = models.CharField(max_length=15)
+    quantity = models.PositiveIntegerField(default=0)
+    img_full = models.ImageField(upload_to="notebook/")
+  
+    
+class desktop_Base(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    price = models.CharField(max_length=15)
+    quantity = models.PositiveIntegerField(default=0)
+    img_full = models.ImageField(upload_to="desktop/")
+
+    
+class console_Base(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=255)
+    price = models.CharField(max_length=15)
+    quantity = models.PositiveIntegerField(default=0)
+    img_full = models.ImageField(upload_to="console/")
+    
