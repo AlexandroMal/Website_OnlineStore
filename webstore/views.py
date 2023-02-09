2b8605894ca865be9fd4ef868b634fbe3e58c1f1
Alexandr Malina <Sashamalina8695@gmail.com>
2023-02-09 23:33:36 +0200

    made a 404 error page

--- webstore/views.py
+++ webstore/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
+from .models import *
 
 # Create your views here.
 
 def index(request): 
-    return render(request, 'webstore/index.html', {'title': 'Webstore'})
+    announcement = telephones_Base.objects.all()
+    return render(request, 'webstore/index.html', {'title': 'Webstore', 'announcement': announcement})
 
 def login(request):
     return render(request, 'webstore/login.html', {'title': 'Sign in'})
