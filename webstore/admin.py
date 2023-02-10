c898525460371f061bbb998a09d5c85f3cd40c6f
Alexandr Malina <Sashamalina8695@gmail.com>
2023-02-10 23:13:17 +0200

    made database mapping in admin mode

--- webstore/admin.py
+++ webstore/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import telephones_Base, notebook_Base, desktop_Base, console_Base
+from .models import product_data, Category
 
-admin.site.register(telephones_Base)
-admin.site.register(notebook_Base)
-admin.site.register(desktop_Base)
-admin.site.register(console_Base)
+admin.site.register(product_data)
+admin.site.register(Category        )
 
