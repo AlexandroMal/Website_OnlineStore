8e82ce8e2f26d4f4176b4493e66691932d1580a1
Alexandr Malina <Sashamalina8695@gmail.com>
2023-02-10 23:08:35 +0200

    Redesigned database models, made a connection between them

--- webstore/models.py
+++ webstore/models.py
@@ -1,33 +1,22 @@
 from django.db import models
+from django.urls import reverse
 
-class telephones_Base(models.Model):
+class product_data(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=255)
     price = models.CharField(max_length=15)
     quantity = models.PositiveIntegerField(default=0)
     img_full = models.ImageField(upload_to="telephones/")
-   
-    
-class notebook_Base(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-    price = models.CharField(max_length=15)
-    quantity = models.PositiveIntegerField(default=0)
-    img_full = models.ImageField(upload_to="notebook/")
-  
-    
-class desktop_Base(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-    price = models.CharField(max_length=15)
-    quantity = models.PositiveIntegerField(default=0)
-    img_full = models.ImageField(upload_to="desktop/")
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
 
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
     
-class console_Base(models.Model):
-    title = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-    price = models.CharField(max_length=15)
-    quantity = models.PositiveIntegerField(default=0)
-    img_full = models.ImageField(upload_to="console/")
+    def __str__(self):
+        return self.name
     
+    def get_url(self):
+        return reverse ('category', kwargs={'cat_id': self.pk})
