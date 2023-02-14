f7255694170cb9bc7db8a605620f0231fa8c24b1
Alexandr Malina <Sashamalina8695@gmail.com>
2023-02-14 23:58:42 +0200

    set up a more convenient admin panel for adding and editing data

--- webstore/admin.py
+++ webstore/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import product_data, Category
 
-admin.site.register(product_data)
-admin.site.register(Category)
+class product_dataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'quantity', 'img_full', 'category')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', )
+    list_filter = ('price', 'category', 'quantity')
+    prepopulated_fields = {'slug':('title',)}
+        
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+    prepopulated_fields = {'slug':('name',)}
+
+admin.site.register(product_data, product_dataAdmin)
+admin.site.register(Category, CategoryAdmin)
 
