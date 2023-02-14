17107ea7d09155042eefac9b37b27f8ebb6074ed
Alexandr Malina <Sashamalina8695@gmail.com>
2023-02-15 00:03:04 +0200

    added slugs to the address bar to the pages of individual products

--- webstore/models.py
+++ webstore/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.urls import reverse
 
 class product_data(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, unique=True, db_index=True)
     description = models.CharField(max_length=255)
     price = models.CharField(max_length=15)
@@ -13,8 +13,13 @@ class product_data(models.Model):
     def __str__(self):
         return self.title
     
-    def get_url(self):
+    def get_absolute_url(self):
         return reverse ('post', kwargs={'slug_post': self.slug})
+    
+    class Meta:
+        verbose_name = 'All product'
+        ordering = ['title', 'id']
+        
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
@@ -23,5 +28,10 @@ class Category(models.Model):
     def __str__(self):
         return self.name
     
-    def get_url(self):
-        return reverse ('category', kwargs={'cat_id': self.pk})
+    def get_absolute_url(self):
+        return reverse ('category', kwargs={'cat_slug': self.slug})
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['id', 'name']
