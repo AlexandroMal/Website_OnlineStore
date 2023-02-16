769fb18f7a2160bee7b35d6bfbc049e95701cf58
Alexandr Malina <Sashamalina8695@gmail.com>
2023-02-16 19:37:35 +0200

    connected the class to enter the profile

--- webstore/urls.py
+++ webstore/urls.py
@@ -3,8 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', WebstoreHome.as_view(), name="home_page"),
-    path('login/', login_user, name="login_page"),
+    path('login/', LoginUser.as_view(), name="login_page"),
     path('register/', RegisterUser.as_view(), name="registration_page"),
+    path('logout/', logout_user, name="logout"),
     path('category/<slug:cat_slug>/', ShowCategory.as_view(), name="category"),
     path('post/<slug:slug_post>/', ShowPost.as_view(), name="post"),
     
