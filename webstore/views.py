c3fc56a671290f72128251fc09b833d7b480c270
Alexandr Malina <Sashamalina8695@gmail.com>
2023-02-16 19:36:35 +0200

    added a class for logging into your profile with automatic redirection to the main page upon successful login

--- webstore/views.py
+++ webstore/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import  HttpResponseNotFound
 from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout, login
 from .models import *
 from .forms import *
 
@@ -24,6 +26,7 @@ class ShowCategory(ListView):
     def get_queryset(self):
         return product_data.objects.filter(category__slug=self.kwargs['cat_slug'])
 
+
 class ShowPost(DetailView):
     model = product_data
     template_name = 'webstore/post.html'
@@ -35,16 +38,39 @@ class ShowPost(DetailView):
         context['title'] = context['post']
         return context
     
+    
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
     template_name = 'webstore/registration.html'
     success_url = reverse_lazy('login_page')
+    
+    def get_context_data(self,*, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Sign Up'
+        return context
+    
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request,user)
+        return redirect('home_page')
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'webstore/login.html'
+    
+    def get_context_data(self,*, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Sign In'
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('home_page')
 
 
+def logout_user(request):
+    logout(request)
+    return redirect('home_page')
 
-
-def login_user(request):
-    return render(request, 'webstore/login.html', {'title': 'Sign in'})
 
 def pageNotFound(request,exception):
     return HttpResponseNotFound('<h1>Page not found</h1>')
